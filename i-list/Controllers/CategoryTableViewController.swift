3a712b01c646eadcf0a8fccbea56aaf4df54ba9a
Abdullah Ali Shah <abdullahalishah@icloud.com>
2018-04-05 21:35:12 +0500

    Core Data Completed

--- i-list/Controllers/CategoryTableViewController.swift
+++ i-list/Controllers/CategoryTableViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import CoreData
+import SwipeCellKit
+import ChameleonFramework
 
 class CategoryTableViewController: UITableViewController {
 
@@ -17,6 +19,8 @@ class CategoryTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         loadCategories()
+        tableView.separatorStyle = .none
+        tableView.rowHeight = 80.0
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,9 +36,12 @@ class CategoryTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cate", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cate", for: indexPath) as! SwipeTableViewCell
         let item = categories[indexPath.row]
         cell.textLabel?.text = item.name
+        cell.delegate = self
+        //cell.backgroundColor = UIColor.randomFlat
+        
         return cell
     }
     
@@ -50,8 +57,6 @@ class CategoryTableViewController: UITableViewController {
             destinationVC.selectedCategory = categories[indexPath.row]
         }
     }
-    
-    
     
     // MARK: - Data Manipulation Methods
     
@@ -98,4 +103,31 @@ class CategoryTableViewController: UITableViewController {
         
         present(alert, animated: true, completion: nil)
     }
+}
+
+// MARK: - Swipe Cell Delegate Methods
+extension CategoryTableViewController: SwipeTableViewCellDelegate {
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> [SwipeAction]? {
+        guard orientation == .right else { return nil }
+        let deleteAction = SwipeAction(style: .destructive, title: "Delete") { action, indexPath in
+            // handle action by updating model with deletion
+            self.context.delete(self.categories[indexPath.row])
+            self.categories.remove(at: indexPath.row)
+            self.saveCategories()
+            tableView.reloadData()
+        }
+        // customize the action appearance
+        // deleteAction.image = UIImage(named: "delete")
+        
+        return [deleteAction]
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeTableOptions {
+        var options = SwipeTableOptions()
+        options.expansionStyle = .destructive
+        options.transitionStyle = .border
+        return options
+    }
+    
 }
--- i-list/Controllers/PrimaryViewController.swift
+++ i-list/Controllers/PrimaryViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 import CoreData
-
+import SwipeCellKit
 class PrimaryViewController: UITableViewController {
     
     var itemArray = [Item]()
@@ -35,10 +35,11 @@ class PrimaryViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! SwipeTableViewCell
         let item = itemArray[indexPath.row]
         cell.textLabel?.text = item.title
         cell.accessoryType = item.done ? .checkmark : .none
+        cell.delegate = self
         return cell
     }
     
@@ -104,8 +105,8 @@ class PrimaryViewController: UITableViewController {
     }
 }
 
-//MARK: - Search bar methods
-extension PrimaryViewController: UISearchBarDelegate {
+//MARK: - Search bar methods and Swipe methods
+extension PrimaryViewController: UISearchBarDelegate, SwipeTableViewCellDelegate {
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         let request : NSFetchRequest<Item> = Item.fetchRequest()
@@ -122,8 +123,28 @@ extension PrimaryViewController: UISearchBarDelegate {
             }
         }
     }
-
     
-
-
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> [SwipeAction]? {
+        guard orientation == .right else { return nil }
+        let deleteAction = SwipeAction(style: .destructive, title: "Delete") { action, indexPath in
+            // handle action by updating model with deletion
+            self.context.delete(self.itemArray[indexPath.row])
+            self.itemArray.remove(at: indexPath.row)
+            self.saveItems()
+            tableView.reloadData()
+        }
+        // customize the action appearance
+        // deleteAction.image = UIImage(named: "delete")
+        
+        return [deleteAction]
+    }
+    
+    /*func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeTableOptions {
+        var options = SwipeTableOptions()
+        options.expansionStyle = .destructive
+        //options.transitionStyle = .border
+        return options
+    }*/
 }
+
+
